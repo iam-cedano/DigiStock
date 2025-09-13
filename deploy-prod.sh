cc9241c41f8efdc73164fcab06e8dde1aeefc639
Oscar Cedano <cedano@engineer.com>
2025-09-13 01:01:28 -0700

    feat: script for deploy to production

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,4 +1,21 @@
 #! /bin/sh
 
-git pull origin main
-cd ./dev/;
+container_base=$(basename $(pwd) | tr '[:upper:]' '[:lower:]');
+service_name="$container_base-workspace-1";
+
+git pull origin main;
+
+docker compose down
+
+docker compose -f docker-compose.prod.yaml down 1>/dev/null;
+
+rsync -aI --exclude-from='.php-fpm.rsync_ignore' ./dev/php-fpm/project/ ./prod/php-fpm/project/;
+
+docker compose -f docker-compose.prod.yaml up -d --build 1>/dev/null;
+
+docker exec $service_name mkdir -p ./prod/php-fpm/project/storage/ 1>/dev/null;
+docker exec $service_name mkdir -p ./prod/php-fpm/project/vendor/ 1>/dev/null;
+docker exec $service_name mkdir -p ./prod/php-fpm/project/node_modules/ 1>/dev/null;
+
+docker exec $service_name php artisan key:generate 1>/dev/null;
+docker exec $service_name npm install 1>/dev/null;
